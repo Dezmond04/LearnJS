e90db70d762db93015b8eab01c612c94ff9a2c41
Dezmond04 <Dezmond04@mail.ru>
2022-03-26 21:33:25 +0300

    added 2 lesson

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 'use scrict';
 
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = 'Data types',
+    screens = 'Простые, Сложные, Интерактивные',
+    screenPrice = 200,
+    rollback = 15,
+    fullPrice = 2500,
+    adaptive = true;
 
-alert('Hello!');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log('Hello, world!');
+console.log(screens.length);
+
+console.log('Стоимость разработки экранов ' + screenPrice + ' долларов');
+
+console.log('Стоимость разработки сайта ' + fullPrice + ' долларов');
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log(fullPrice * (rollback/100));
+
+
+
+
+
+
+
